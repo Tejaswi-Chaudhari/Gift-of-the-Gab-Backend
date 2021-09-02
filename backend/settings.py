5e652b3a017a699919340bd4aea8958f31e0a186
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-09-02 23:15:27 +0530

    static

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-3v+97%=i%dy&(ty6ci6lc)!zk3th)9wram^v*_++p=p+_-bh!u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -134,6 +134,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
